8f94faf1a673bec7e0ee6da2daec6d3da6ecd8df
andrevsl <andrevsl@gmail.com>
2019-12-11 14:47:20 -0300

    Menu item and Event Add

--- src/app/restaurants/restaurant-detail/menu/menu.component.ts
+++ src/app/restaurants/restaurant-detail/menu/menu.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RestaurantsService} from '../../restaurant.service'
+import { Observable } from 'rxjs';
+import {ActivatedRoute} from '@angular/router'
+import { MenuItem } from '../menu-item/menu-item.model';
 @Component({
   selector: 'mt-menu',
   templateUrl: './menu.component.html'
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+
+  menu: Observable<MenuItem []>
+
+  constructor(private restaurantsService:RestaurantsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+      this.menu=this.restaurantsService
+        .menuOfRestaurant(this.route.parent.snapshot.params['id'])
+  // parent, hence being manu, a child node
+  }
+
+  addMenuItem(item:MenuItem){
+    console.log(item)
   }
 
 }
