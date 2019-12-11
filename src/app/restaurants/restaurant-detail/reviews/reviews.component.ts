82d6c0e7263bb57db3ed849093dbc99bc3d4ec3e
andrevsl <andrevsl@gmail.com>
2019-12-11 00:49:54 -0300

    Route Children - Menu Reviews server

--- src/app/restaurants/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurants/restaurant-detail/reviews/reviews.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RestaurantsService} from '../../restaurant.service'
+import { Observable } from 'rxjs';
+import {ActivatedRoute} from '@angular/router'
 @Component({
   selector: 'mt-reviews',
   templateUrl: './reviews.component.html'
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<any>
+
+  constructor(private restaurantsService:RestaurantsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.reviews=this.restaurantsService
+    .reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
+  // parent, hence being reviews a child node
   }
 
 }
