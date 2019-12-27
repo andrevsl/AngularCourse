f944d776e464c458079203f602d3f2fc183a5996
andrevsl <andrevsl@gmail.com>
2019-12-27 14:12:49 -0300

    Search Bar SwitchMap

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {trigger, state, style, transition, animate} from '@angular/animations'
-
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms'
 import {Restaurant} from './restaurant/restaurant.model'
 import {RestaurantsService} from "./restaurant.service"
+
+import 'rxjs/add/operator/switchMap'
+
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
@@ -27,12 +30,27 @@ export class RestaurantsComponent implements OnInit {
   searchBarState='hidden'
   restaurants:Restaurant []
 
-  constructor(private restaurantsService:RestaurantsService) { }
+  searchForm: FormGroup
+  searchControl: FormControl
+  constructor(private restaurantsService:RestaurantsService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.restaurantsService.restaurants()
+    this.searchControl=this.fb.control('')
+    this.searchForm=this.fb.group({
+      searchControl: this.searchControl
+    })
+
+      
+    this.searchControl.valueChanges
+            .switchMap(searchTerm=> 
+              this.restaurantsService.restaurants(searchTerm))
+            .subscribe(restaurants =>this.restaurants=restaurants)
+
+   this.restaurantsService.restaurants()
       .subscribe(restaurants=>this.restaurants=restaurants)
   }
+
 
   toggleSearch(){
     this.searchBarState=this.searchBarState=== 'hidden' ? 'visible' : 'hidden'
