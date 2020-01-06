cca2e8ad971e4a06487c7579b6b7eef4f0e0f023
andrevsl <andrevsl@gmail.com>
2020-01-06 13:03:52 -0300

    initial commit

---
+++ rating-custom-element/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RatingComponent } from './rating.component';
+
+@NgModule({
+  declarations: [
+    RatingComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [RatingComponent]
+})
+export class AppModule { }
---
+++ rating-custom-element/src/app/rating.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rc-root',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
+})
+export class RatingComponent {
+  title = 'rating-custom-element';
+}
